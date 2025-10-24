e694c88b5bb4a01cab7eda40c19029b4f94a5bed
akshatsri47 <ak.sr2201@gmail.com>
2025-10-25 00:39:04 +0530

    jenking version-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,20 +14,9 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# Data source for latest Ubuntu AMI
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-lts-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+# Using specific Ubuntu AMI ID
+locals {
+  ami_id = "ami-0036347a8a8be83f1"
 }
 
 # Security Group for Chess Application
@@ -117,7 +106,7 @@ resource "aws_iam_instance_profile" "chess_instance_profile" {
 
 # EC2 Instance
 resource "aws_instance" "chess_app" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = local.ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.chess_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.chess_instance_profile.name
