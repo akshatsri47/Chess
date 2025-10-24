544000f07ba026de069e3463595155d96067a232
akshatsri47 <ak.sr2201@gmail.com>
2025-10-25 00:16:16 +0530

    jenking version-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -116,29 +116,4 @@ resource "aws_eip" "chess_eip" {
   }
 }
 
-# Outputs
-output "instance_ip" {
-  description = "Public IP address of the Chess application instance"
-  value       = aws_eip.chess_eip.public_ip
-}
-
-output "instance_id" {
-  description = "ID of the Chess application instance"
-  value       = aws_instance.chess_app.id
-}
-
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.chess_sg.id
-}
-
-output "frontend_url" {
-  description = "Frontend URL"
-  value       = "http://${aws_eip.chess_eip.public_ip}:5173"
-}
-
-output "backend_websocket_url" {
-  description = "Backend WebSocket URL"
-  value       = "ws://${aws_eip.chess_eip.public_ip}:8181"
-}
 
